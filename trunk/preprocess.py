0d34f4e4c4ef5d8dede39ab3412d015a25a77822
silvia.laura.pintea <silvia.laura.pintea@fb641738-5793-2f27-248a-c372f8ce5f8b>
2010-06-01 20:56:41 +0000

    try a start up

---
+++ trunk/preprocess.py
@@ -0,0 +1,26 @@
+"""Class for all the preprocessing on the images => center the hand in the middle, scale"""
+
+import sys
+import urllib
+import re
+import copy
+import math
+import random
+"""from some_class import *"""
+
+class preprocessing:
+	def __init__(self):
+		dirImages   = ""
+		scaleWidth  = 200
+		scaleHeight = 200
+	"""_________________________________________________________________"""
+	"""does the scaling of all the images that it reads from a direcotry"""
+	def scale(self):
+		print "Nothing -- scale ;)"
+	"""_________________________________________________________________"""
+	"""centers the hands in the images"""
+	def center(self):
+		print "Nothing -- center ;)"
+"""_________________________________________________________________________"""
+pre = preprocessing()
+pre.center()
