efcace4be780d2c46958c8547f5f1da0846bf648
nraiman <nraiman@fb641738-5793-2f27-248a-c372f8ce5f8b>
2010-06-03 15:33:29 +0000

    skincolor detector improved, searches once for skin color and stores it

--- trunk/facedetect.py
+++ trunk/facedetect.py
@@ -24,7 +24,13 @@ haar_scale = 1.2
 min_neighbors = 2
 haar_flags = 0
 
-class getHistValues:
+# variables to store average hue and saturation
+global H
+global S
+
+H=S=0
+
+class getHistValues: # not used right now
     def __init__(self,img,roi=None):
         if roi:
             cv.SetImageROI( img._hue, roi );
@@ -47,49 +53,49 @@ class getHistValues:
 
         return values;
 
+class getSkinColor:
+    def __init__(self,img):
 
-class detect_and_draw:
-    def __init__(self,img,cascade):
-            
+        cascade = cv.Load("haarcascades\haarcascade_frontalface_alt.xml")
+
         rect = (0,0,1,1)
 
         # allocate temporary images
         gray = cv.CreateImage((img.width,img.height), 8, 1)
-        small_img = cv.CreateImage((cv.Round(img.width / image_scale),
-                       cv.Round (img.height / image_scale)), 8, 1)
+#        small_img = cv.CreateImage((cv.Round(img.width / image_scale),
+#                       cv.Round (img.height / image_scale)), 8, 1)
 
         # convert color input image to grayscale
         cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
 
         # scale input image for faster processing
-        cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
+#        cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
 
-        cv.EqualizeHist(small_img, small_img)
+        cv.EqualizeHist(gray, gray)
+        
+        faces = cv.HaarDetectObjects(gray, cascade, cv.CreateMemStorage(0),
+                                     haar_scale, min_neighbors, haar_flags, min_size)
+        if faces:
+            for ((x, y, w, h), n) in faces:
 
-        if(cascade):
-            t = cv.GetTickCount()
-            faces = cv.HaarDetectObjects(small_img, cascade, cv.CreateMemStorage(0),
-                                         haar_scale, min_neighbors, haar_flags, min_size)
-            t = cv.GetTickCount() - t
-            print "detection time = %gms" % (t/(cv.GetTickFrequency()*1000.))
-            if faces:
-                for ((x, y, w, h), n) in faces:
+                print "(x, y, w, h) = (%d, %d, %d, %d)" % (x, y, w, h)
+                print "width is %d and height is %d" % (img.width, img.height)
+
+                faceX = x
+                faceY = y
+                faceW = w
+                faceH = h
+                
+                reScale = 0.2
+                horScl = int(faceW * reScale)
+                verScl = int(faceH * reScale)
                     
-                    faceX = int(x * image_scale)
-                    faceY = int(y * image_scale)
-                    faceW = w * image_scale
-                    faceH = h * image_scale
-                    
-                    reScale = 0.2
-                    horScl = int(faceW * reScale)
-                    verScl = int(faceH * reScale)
-                    
-                    rect = (faceX+horScl,faceY+verScl,faceW-(horScl*2),faceH-(verScl*2))
-                    # the input to cv.HaarDetectObjects was resized, so scale the 
-                    # bounding box of each face and convert it to two CvPoints
-                    pt1 = (int(x * image_scale), int(y * image_scale))
-                    pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
-                    cv.Rectangle(img, pt1, pt2, cv.RGB(255, 0, 0), 3, 8, 0)
+                rect = (faceX+horScl,faceY+verScl,faceW-(horScl*2),faceH-(verScl*2))
+                # the input to cv.HaarDetectObjects was resized, so scale the 
+                # bounding box of each face and convert it to two CvPoints
+                pt1 = (int(x), int(y))
+                pt2 = (int((x + w)), int((y + h)))
+                cv.Rectangle(img, pt1, pt2, cv.RGB(255, 0, 0), 3, 8, 0)
 
 
         cv.ShowImage("result", img)
@@ -105,11 +111,6 @@ class detect_and_draw:
 
         faceHSV = cv.CreateImage(cv.GetSize(face), 8, 3)
         cv.CvtColor(face, faceHSV, cv.CV_BGR2HSV);
-        imgHSV = cv.CreateImage(cv.GetSize(img), 8, 3)
-        cv.CvtColor(img, imgHSV, cv.CV_BGR2HSV);
-
-        hsv = cv.CreateImage(cv.GetSize(img),img.depth,img.nChannels);
-        cv.CvtColor(face, faceHSV, cv.CV_BGR2HSV);
 
         hueFace = cv.CreateMat(face.height, face.width, cv.CV_8UC1)
         satFace = cv.CreateMat(face.height, face.width, cv.CV_8UC1)
@@ -118,24 +119,18 @@ class detect_and_draw:
 
         cv.ShowImage("hue", hueFace)
 
-        hueImg = cv.CreateMat(img.height, img.width, cv.CV_8UC1)
-        satImg = cv.CreateMat(img.height, img.width, cv.CV_8UC1)
-        valImg = cv.CreateMat(img.height, img.width, cv.CV_8UC1)
-        cv.Split(imgHSV, hueImg, satImg, valImg, None)
-
-        cv.ShowImage("hueImg", hueImg)
-
         #intitialize value to store average Hue value
-        H = 0
-        S = 0
-        V = 0
         maxi=0
         mini=1000
+
+        global H
+        global S
+        H = S = 0
         for x in range(0, hueFace.height):
             for y in range(0, hueFace.width):
                 H = H + hueFace[x,y]
                 S = S + satFace[x,y]
-                V = V + valFace[x,y]
+#                V = V + valFace[x,y]
                 if hueFace[x,y]>maxi:
                     maxi=hueFace[x,y]
                 if hueFace[x,y]<mini:
@@ -145,44 +140,63 @@ class detect_and_draw:
             numPixels = hueFace.height * hueFace.width
             H = H/numPixels
             S = S/numPixels
-            V = V/numPixels
+#            V = V/numPixels
+            print "skin color (H, S) = (%f, %f)" % (H,S)
 
-        print "average (h s v) is: (%f %f %f)" % (H, S, V)
-        print "max is: %f   mim is: %f" % (maxi, mini)
+
+class detect_and_draw:
+    def __init__(self,img):
+        
+        small_img = cv.CreateImage((cv.Round(img.width / image_scale),cv.Round(img.height / image_scale)), 8, 3)
+        cv.Resize(img, small_img, cv.CV_INTER_LINEAR)
+
+        if H==0 and S ==0:
+            getSkinColor(small_img)
+
+        imgHSV = cv.CreateImage(cv.GetSize(small_img), 8, 3)
+        cv.CvtColor(small_img, imgHSV, cv.CV_BGR2HSV);
+
+        hueImg = cv.CreateMat(small_img.height, small_img.width, cv.CV_8UC1)
+        satImg = cv.CreateMat(small_img.height, small_img.width, cv.CV_8UC1)
+        valImg = cv.CreateMat(small_img.height, small_img.width, cv.CV_8UC1)
+        cv.Split(imgHSV, hueImg, satImg, valImg, None)
+
+        cv.ShowImage("hueImg", hueImg)
 
         hueTrshld = cv.CreateMat(hueImg.height, hueImg.width, cv.CV_8UC1)
+        hueDiff = 30
+        satDiff = 80
         for x in range(0, hueTrshld.height):
             for y in range(0, hueTrshld.width):
-                if hueImg[x,y]>(H-30) and hueImg[x,y]>(2) and hueImg[x,y]<(H+30) and satImg[x,y]>(S) and satImg[x,y]<(S+30):
-                    hueTrshld[x,y] = 255
-                else:
-                    hueTrshld[x,y] = 0
+                hueTrshld[x,y] = 0
+                if hueImg[x,y]>(H-hueDiff) and hueImg[x,y]>(1) and hueImg[x,y]<(H+hueDiff):
+                    if satImg[x,y]>(S-satDiff) and satImg[x,y]<(S+satDiff):
+                        hueTrshld[x,y] = 255
 
         hueTrshldErode = cv.CreateMat(hueImg.height, hueImg.width, cv.CV_8UC1)        
         hueTrshldDilate = cv.CreateMat(hueImg.height, hueImg.width, cv.CV_8UC1)        
 
 
-#        kernel10 = cv.CreateStructuringElementEx(10,10,0,0, cv.CV_SHAPE_RECT)
-#        kernel6 = cv.CreateStructuringElementEx(6,6,0,0, cv.CV_SHAPE_RECT)
-#        cv.Erode(hueTrshld, hueTrshldErode, kernel6, 1)
-#        cv.Dilate(hueTrshldErode, hueTrshldDilate, kernel6, 1)
+        kernel10 = cv.CreateStructuringElementEx(10,10,0,0, cv.CV_SHAPE_RECT)
+        kernel8 = cv.CreateStructuringElementEx(8,8,0,0, cv.CV_SHAPE_RECT)
+        kernel6 = cv.CreateStructuringElementEx(6,6,0,0, cv.CV_SHAPE_RECT)
+        kernel4 = cv.CreateStructuringElementEx(4,4,0,0, cv.CV_SHAPE_RECT)
+
+        cv.Erode(hueTrshld, hueTrshldErode, kernel6, 1)
+        cv.Dilate(hueTrshldErode, hueTrshldDilate, kernel10, 1)
 
         
-        cv.ShowImage("hueTrshld", hueTrshld)
-#        cv.ShowImage("hueTrshld", hueTrshldDilate)
-
-#        print getHistValues(img)
+        cv.ShowImage("hueTrshldOr", hueTrshld) #original
+        cv.ShowImage("hueTrshldDi", hueTrshldDilate) #dilated
+        cv.ShowImage("hueTrshldEr", hueTrshldErode)  #eroded
 
 if __name__ == '__main__':
 
     parser = OptionParser(usage = "usage: %prog [options] [filename|camera_index]")
-#    parser.add_option("-c", "--cascade", action="store", dest="cascade", type="str", help="Haar cascade file, default %default", default = "C:\OpenCV2.1\data\haarcascades/haarcascade_frontalface_alt.xml")
     parser.add_option("-c", "--cascade", action="store", dest="cascade", type="str", help="Haar cascade file, default %default", default = "haarcascades\haarcascade_frontalface_alt.xml")
     (options, args) = parser.parse_args()
 
-    print options.cascade
-    
-    cascade = cv.Load(options.cascade)
+#    cascade = cv.Load(options.cascade)
     
     if len(args) != 1:
         parser.print_help()
@@ -198,11 +212,14 @@ if __name__ == '__main__':
     cv.NamedWindow("resultFace", cv.CV_WINDOW_AUTOSIZE)
     cv.NamedWindow("hue", cv.CV_WINDOW_AUTOSIZE)
     cv.NamedWindow("hueImg", cv.CV_WINDOW_AUTOSIZE)
-    cv.NamedWindow("hueTrshld", cv.CV_WINDOW_AUTOSIZE)
+    cv.NamedWindow("hueTrshldOr", cv.CV_WINDOW_AUTOSIZE)
+    cv.NamedWindow("hueTrshldEr", cv.CV_WINDOW_AUTOSIZE)
+    cv.NamedWindow("hueTrshldDi", cv.CV_WINDOW_AUTOSIZE)
 
     if capture:
         frame_copy = None
         while True:
+            t = cv.GetTickCount() #start timer
             frame = cv.QueryFrame(capture)
             if not frame:
                 cv.WaitKey(0)
@@ -215,10 +232,13 @@ if __name__ == '__main__':
             else:
                 cv.Flip(frame, frame_copy, 0)
             
-            detect_and_draw(frame_copy, cascade)
+            detect_and_draw(frame_copy)
 
             if cv.WaitKey(10) >= 0:
                 break
+            #stop timer and show elapsed time
+            t = cv.GetTickCount() - t
+            print "detection time = %gms" % (t/(cv.GetTickFrequency()*1000.))
     else:
         image = cv.LoadImage(input_name, 1)
         detect_and_draw(image, cascade)
@@ -227,5 +247,8 @@ if __name__ == '__main__':
     cv.DestroyWindow("result")
     cv.DestroyWindow("resultFace")
     cv.DestroyWindow("hue")
-
+    cv.DestroyWindow("hueImg")
+    cv.DestroyWindow("hueTrshldOr")
+    cv.DestroyWindow("hueTrshldEr")
+    cv.DestroyWindow("hueTrshldDi")
 
