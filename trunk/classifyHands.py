adf5ce322e91529720cc4b151c88a4abcb61eb1e
silvia.laura.pintea <silvia.laura.pintea@fb641738-5793-2f27-248a-c372f8ce5f8b>
2010-06-08 18:25:31 +0000

    hands classfieir - svn with libsvn still not working

--- trunk/classifyHands.py
+++ trunk/classifyHands.py
@@ -27,9 +27,9 @@ class classifyHands:
 		self.pca = eigenHands()
 		#create the data matrix if they are not there
 		self.pca.makeMatrix("test")
+		#self.pca.makeMatrix("hands")
 		if(makeData == True):
 			self.pca.makeMatrix("hands")
-			self.pca.makeMatrix("garb")
 			self.pca.makeMatrix("rock")
 			self.pca.makeMatrix("paper")
 			self.pca.makeMatrix("scissors")
@@ -45,17 +45,14 @@ class classifyHands:
 	#get the training set from video of hands
 	def classifyHands(self, noComp):
 		#0) noComp needs to be the same for all sets 	
-		hands, _, _ = self.pca.doPCA("hands", noComp)
-		garb, _, _  = self.pca.doPCA("garb", noComp)
+		hands,dataH,liH,meanH = self.pca.doPCA("hands", noComp, -1)
+		test,dataT,liT,meanT  = self.pca.doPCA("test", noComp, -1)
 		
 		#1) define the problem: labels & samples 
 		labels  = []
 		samples = []
-		for i in range(0, hands.shape[0]):
-			samples.append(hands[i,:].tolist())
-			labels.append(1)
-		for j in range(0, garb.shape[0]):
-			samples.append(garb[j,:].tolist())
+		for i in range(0, hands.shape[1]):
+			samples.append(hands[0:noComp, i].tolist())
 			labels.append(0)
 
 		#2) store the problem :-/
@@ -63,25 +60,36 @@ class classifyHands:
 		size    = len(samples)
 
 		#3) generate the model from the data 
-		#kernels = [svm.LINEAR, svm.POLY, svm.RBF, svm.SIGMOID, svm.EPSILON_SVR, svm.NU_SVR, svm.NU_SVC, svm.C_SVC]
-		param = svm.svm_parameter(kernel_type = svm.RBF, probability = 1)
+		#types   = [svm.NU_SVR, svm.NU_SVC, svm.C_SVC, svm.ONE_CLASS, svm.EPSILON_SVR]
+		#kernels = [svm.LINEAR, svm.POLY, svm.RBF, svm.SIGMOID, svm.PRECOMPUTED]
+		param = svm.svm_parameter(svm_type = svm.ONE_CLASS, kernel_type = svm.RBF)
 		model = svm.svm_model(problem, param)
 
 		#4) make some prediction :-s
-		test, _, _ = self.pca.doPCA("test", noComp)
-		for i in range(0, test.shape[0]):
-			pred_label, pred_probability = model.predict_probability(test[i].tolist())
-			if(pred_label == 1):
-				labelName = "hands"
-			else:
-				labelName = "garb"
-			print "label:"+str(labelName)+"["+str(pred_label)+"] >>> prob:"+str(pred_probability)
+		groundTruth = [1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+		error       = 0.0
+		for i in range(0, test.shape[1]):
+			prediction = model.predict(test[0:noComp, i].tolist())
+			if(int(prediction) != int(groundTruth[i])):
+				error += 1.0 
+			print prediction
+
+		error = float(error)/float(len(groundTruth))
+		print error			
+		print noComp
 		return model
 	#________________________________________________________________________
 	#get the training set from video of hands
 	def classifySigns(self):
 		""".."""
+		#pred_label, pred_probability = model.predict_probability(test[i, 0:noComp].tolist())
+		#if(pred_label == 1):
+		#	labelName = "hands"
+		#else:
+		#	labelName = "garb"
+		#print "label:"+str(labelName)+"["+str(pred_label)+"] >>> prob:"+str(pred_probability)
+
 #________________________________________________________________________
 classi = classifyHands(False)
-classi.classifyHands(13)
+classi.classifyHands(1000)
 
