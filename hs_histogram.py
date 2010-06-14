41b12c40a9d06c93cee26d3110a281ab418d986b
nraiman <nraiman@fb641738-5793-2f27-248a-c372f8ce5f8b>
2010-06-14 15:54:20 +0000

    train deleted

--- hs_histogram.py
+++ hs_histogram.py
@@ -5,27 +5,21 @@ class hs_histogram:
     def __init__(self, src):
         self.src = src
         self.hist = None
-    def getHist(self):
-        # Convert to HSV
-#        hsv = cv.CreateImage(cv.GetSize(self.src), 8, 3)
-#        cv.CvtColor(self.src, hsv, cv.CV_BGR2HSV)
-
+    def getHist(self, hBins, sBins):
         # Extract the H and S planes
         h_plane = cv.CreateMat(self.src.height, self.src.width, cv.CV_8UC1)
         s_plane = cv.CreateMat(self.src.height, self.src.width, cv.CV_8UC1)
         cv.Split(self.src, h_plane, s_plane, None, None)
         planes = [h_plane, s_plane]
 
-        h_bins = 30
-        s_bins = 32
-        hist_size = [h_bins, s_bins]
+        hist_size = [hBins, sBins]
         # hue varies from 0 (~0 deg red) to 180 (~360 deg red again */
         h_ranges = [0, 180]
         # saturation varies from 0 (black-gray-white) to 255 (pure spectrum color)
         s_ranges = [0, 255]
         ranges = [h_ranges, s_ranges]
-        scale = 10
-        self.hist = cv.CreateHist([h_bins, s_bins], cv.CV_HIST_ARRAY, ranges, 1)
+
+        self.hist = cv.CreateHist([hBins, sBins], cv.CV_HIST_ARRAY, ranges, 1)
         cv.CalcHist([cv.GetImage(i) for i in planes], self.hist)
 
         return self.hist
