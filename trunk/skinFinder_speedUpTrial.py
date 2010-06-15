8c40c2000bbad57624f5f5bea0f591413296048f
nraiman <nraiman@fb641738-5793-2f27-248a-c372f8ce5f8b>
2010-06-15 20:30:08 +0000

    skindetector updated

--- trunk/skinFinder_speedUpTrial.py
+++ trunk/skinFinder_speedUpTrial.py
@@ -18,10 +18,21 @@ if __name__ == '__main__':
 
     imageResize = 0.5
     
+    hasHist = False
+    
+    bestFaceX    = 0
+    bestFaceY    = 0
+    bestFaceW    = 0
+    bestFaceH    = 0
+
     if capture:
         inputFrame = None
         while True:
             frameCount+=1
+            
+            if frameCount%50==0:
+                hasHist = False
+
             t = cv.GetTickCount() #start timer
             frame = cv.QueryFrame(capture)
             if not frame:
@@ -48,66 +59,65 @@ if __name__ == '__main__':
             cv.CvtColor(frameSmall, frameSmallHSV, cv.CV_BGR2HSV)
             ###################################
 
-            ###########face detection##########
-            min_neighbors = 2
-            flags = cv.CV_HAAR_DO_CANNY_PRUNING
-            min_size = (cv.Round(frameSmallGray.height/5),cv.Round(frameSmallGray.width/5))
-            haar_scale = 1.2
-            cascade = cv.Load("haarcascades\haarcascade_frontalface_alt.xml")
-            
-            faces = cv.HaarDetectObjects(frameSmallGray, cascade, cv.CreateMemStorage(0),
-                                         haar_scale, min_neighbors, flags, min_size)
-
-            rect = None
-            biggestFace = 0
-            biggestX    = 0
-            biggestY    = 0
-            biggestW    = 0
-            biggestH    = 0
-            for ((x, y, w, h), n) in faces:
-                if w*h>biggestFace:
-                    biggestFace=w*h
-                    biggestX    = x
-                    biggestY    = y
-                    biggestW    = w
-                    biggestH    = h
-            
-            reScale = 0.3
-            horScl = int(biggestW * reScale)
-            verScl = int(biggestH * reScale)
-                
-            #rect of middle of face
-#                rect = (biggestX+int(biggestW*0.3),biggestY,int(biggestW*0.5),int(biggestH*0.25)) # forhead
-            rect = (biggestX+horScl,biggestY+verScl,biggestW-(horScl*2),biggestH-(verScl*2)) # middle part of face
-
-            pt1 = (int(biggestX), int(biggestY))
-            pt2 = (int((biggestX + biggestW)), int((biggestY + biggestH)))
-            cv.Rectangle(frameSmall, pt1, pt2, cv.RGB(0, 255, 0), 3, 8, 0)
-            ###################################
-            ########calculate histogram########
             hBins = 30
             sBins = 32
 
-            if biggestFace>0: 
-                #sets the Region of Interest (face) in HSV image
-                cv.SetImageROI(frameSmallHSV, rect);
-                face = cv.CreateImage(cv.GetSize(frameSmallHSV),frameSmallHSV.depth,frameSmallHSV.nChannels);
-                cv.Copy(frameSmallHSV, face);
-                cv.ResetImageROI(frameSmallHSV);
-                #get size of face area
-                faceArea = face.height*face.width
+            if hasHist == False:
+                ###########face detection##########
+                min_neighbors = 2
+                flags = cv.CV_HAAR_DO_CANNY_PRUNING
+                min_size = (cv.Round(frameSmallGray.height/5),cv.Round(frameSmallGray.width/5))
+                haar_scale = 1.2
+                cascade = cv.Load("haarcascades\haarcascade_frontalface_alt.xml")
+                
+                faces = cv.HaarDetectObjects(frameSmallGray, cascade, cv.CreateMemStorage(0),
+                                             haar_scale, min_neighbors, flags, min_size)
 
-                hist = hs_histogram(face)
-                myHist = hist.getHist(hBins, sBins)
-                (_, maxValue, _, _) = cv.GetMinMaxHistValue(myHist)
+                rect = None
+                biggestFace = 0
+                for ((x, y, w, h), n) in faces:
+                    if w*h>biggestFace:
+                        biggestFace=w*h
+                        bestFaceX    = x
+                        bestFaceY    = y
+                        bestFaceW    = w
+                        bestFaceH    = h
+                
+                reScale = 0.3
+                horScl = int(bestFaceW * reScale)
+                verScl = int(bestFaceH * reScale)
+                    
+                #rect of middle of face
+                rect = (bestFaceX+horScl,bestFaceY+verScl,bestFaceW-(horScl*2),bestFaceH-(verScl*2)) # middle part of face
 
+                pt1 = (int(bestFaceX), int(bestFaceY))
+                pt2 = (int((bestFaceX + bestFaceW)), int((bestFaceY + bestFaceH)))
+                cv.Rectangle(frameSmall, pt1, pt2, cv.RGB(0, 255, 0), 3, 8, 0)
+                ###################################
+                ########calculate histogram########
+                if biggestFace>0: 
+                    #sets the Region of Interest (face) in HSV image
+                    cv.SetImageROI(frameSmallHSV, rect);
+                    face = cv.CreateImage(cv.GetSize(frameSmallHSV),frameSmallHSV.depth,frameSmallHSV.nChannels);
+                    cv.Copy(frameSmallHSV, face);
+                    cv.ResetImageROI(frameSmallHSV);
+                    #get size of face area
+                    faceArea = face.height*face.width
+
+                    hist = hs_histogram(face)
+                    myHist = hist.getHist(hBins, sBins)
+                    (_, maxValue, _, _) = cv.GetMinMaxHistValue(myHist)
+                    print "face detected >>> Histogram constructed"
+                    hasHist = True
+                ###################################
+
+            else: # if hist already calculated
                 skinProbImg = cv.CreateImage(cv.GetSize(frameSmallHSV),8,1);
-
-                hueImg = cv.CreateMat(frameSmallHSV.height, frameSmallHSV.width, cv.CV_8UC1)
-                satImg = cv.CreateMat(frameSmallHSV.height, frameSmallHSV.width, cv.CV_8UC1)
+                hueImg      = cv.CreateMat(frameSmallHSV.height, frameSmallHSV.width, cv.CV_8UC1)
+                satImg      = cv.CreateMat(frameSmallHSV.height, frameSmallHSV.width, cv.CV_8UC1)
                 cv.Split(frameSmallHSV, hueImg, satImg, None, None)
 
-                binSum=face.height*face.width
+                binSum = face.height*face.width
                 maxProbInt = 0
                 for x in range(0, frameSmallHSV.height):
                     for y in range(0, frameSmallHSV.width):
@@ -116,46 +126,38 @@ if __name__ == '__main__':
                             hue =hBins-1
                         sat = int(satImg[x,y]/(256/sBins))
 
-                        binVal = cv.QueryHistValue_2D(myHist, hue, sat)
-                        binProb = binVal/binSum
-                        probIntensity = int(binProb * 255 / (maxValue/binSum))
-                        
+                        binVal           = cv.QueryHistValue_2D(myHist, hue, sat)
+                        binProb          = binVal/binSum
+                        probIntensity    = int(binProb * 255 / (maxValue/binSum))
                         skinProbImg[x,y] = probIntensity
                         
                         if probIntensity>maxProbInt:
                             maxProbInt=probIntensity
-                
                 cv.ShowImage("skinProb", skinProbImg) #Original skin probability image
 
                 #threshold
                 cv.InRangeS(skinProbImg,100,255,skinProbImg)
                 cv.ShowImage("skinProbThresholded1", skinProbImg) #Original skin probability image after thresholding
-                
                 #smooth
-                cv.Smooth(skinProbImg,skinProbImg)
+                cv.Smooth(skinProbImg,skinProbImg,cv.CV_BLUR_NO_SCALE)
                 cv.ShowImage("skinProbSmoothed", skinProbImg) #Original skin probability image after thresholding
-
                 #erode
                 kernelEr = cv.CreateStructuringElementEx(4,4,0,0, cv.CV_SHAPE_RECT)
                 cv.Erode(skinProbImg, skinProbImg, kernelEr, 1)
                 cv.ShowImage("skinProbEroded", skinProbImg) #Original skin probability image after thresholding
-                
                 #dilate
-                kernelDi = cv.CreateStructuringElementEx(15,18,0,0, cv.CV_SHAPE_RECT)
+                kernelDi = cv.CreateStructuringElementEx(14,14,0,0, cv.CV_SHAPE_ELLIPSE)
                 cv.Dilate(skinProbImg, skinProbImg, kernelDi, 1)
                 cv.ShowImage("skinProbDilated", skinProbImg) #Original skin probability image after thresholding
-
                 #smooth
-                cv.Smooth(skinProbImg,skinProbImg,cv.CV_BLUR_NO_SCALE)
                 cv.Smooth(skinProbImg,skinProbImg,cv.CV_BLUR_NO_SCALE)
                 cv.ShowImage("skinProbSmoothed", skinProbImg) #Original image
 
+                #construct color image to add colored contour and rectangle
                 skinProbColor = cv.CreateImage((skinProbImg.width,skinProbImg.height), 8, 3)
                 cv.CvtColor(skinProbImg, skinProbColor, cv.CV_GRAY2BGR)
-
                 # find contours for blobs
-                contours = cv.CreateMemStorage()
-                seq = cv.FindContours(skinProbImg, contours, cv.CV_RETR_EXTERNAL);
+                seq = cv.FindContours(skinProbImg, cv.CreateMemStorage(), cv.CV_RETR_EXTERNAL);
                 while seq:
                     cv.DrawContours(skinProbColor,seq,(255,0,0),(255,255,0),0,2)
                     seqRect = cv.BoundingRect(seq)
@@ -163,9 +165,20 @@ if __name__ == '__main__':
                     y = seqRect[1]
                     w = seqRect[2]
                     h = seqRect[3]
-                    cv.Rectangle(skinProbColor, (x,y), (x+w,y+h), cv.RGB(0, 255, 0), 3, 8, 0)
+                    
+                    goodRectCntr = 0
+                    goodRects=[10]
+                    if w>=20 and h>=20:
+                        rectCntrX = x + int(w/2)
+                        rectCntrY = y + int(h/2)
+                        if not((rectCntrX>bestFaceX and rectCntrX<bestFaceX+bestFaceW) and(rectCntrY>bestFaceY and rectCntrY<bestFaceY+bestFaceH)):
+                            #rect is not in face area
+                            cv.Rectangle(skinProbColor, (x,y), (x+w,y+h), cv.RGB(0, 255, 0), 3, 8, 0)
+                            goodRects[goodRectCntr] = (x,y,h,w)
+                            goodRectCntr+=1
                     seq = seq.h_next()
                 cv.ShowImage("skinProbColor", skinProbColor) #blobs with contours
+                print goodRectCntr," usefull rectangels found"
 
             cv.ShowImage("input", frameSmall) #Original image
             
@@ -173,7 +186,6 @@ if __name__ == '__main__':
             totalTime += t/(cv.GetTickFrequency()*1000.)
             if frameCount%10==0:
                 print "after %i frames the average time = %gms" % (frameCount, totalTime/frameCount)
-            
             if cv.WaitKey(10) >= 0:
                 break
-            
+        
