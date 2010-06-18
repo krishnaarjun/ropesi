ef9e98cd4dc1a8b5508773209232fce4cde51051
silvia.laura.pintea <silvia.laura.pintea@fb641738-5793-2f27-248a-c372f8ce5f8b>
2010-06-18 21:05:51 +0000

    skin detection changed to classify both (hands vs garbage) and (rock&paper&scissors)

--- skinFinder.py
+++ skinFinder.py
@@ -8,8 +8,9 @@ from predictSign import *
 class detectSkin:
     def __init__(self):
 	#load the model of the classification
-	self.predict = predictSign(70) # takes the size of the images as input
-	problem      = self.predict.loadModel("knn", 1) # 1=>original images; 2=>PCA; 3=>Gabor Wavelets+original image; 4=>only Gabor Wavelets	
+	self.predict = predictSign(70, False) # takes the size of the images as input
+	problem_sign = self.predict.loadModel("knn", 1, "rock") # 1=>original images; 2=>PCA; 3=>Gabor Wavelets+original image; 4=>only Gabor Wavelets	
+	problem_hand = self.predict.loadModel("knn", 1, "hands") # 1=>original images; 2=>PCA; 3=>Gabor Wavelets+original image; 4=>only Gabor Wavelets	
 	self.goalImg = cv.CreateImage((70,70), cv.IPL_DEPTH_8U, 1)
 
 	#skin detector from now on 
@@ -111,7 +112,9 @@ class detectSkin:
                     print "after %i frames the average time = %gms" % (frameCount, totalTime/frameCount)
 
 		#does the prediction on the given image of hand
-		self.predict.doPrediction(1, "knn", problem, "rock", self.goalImg)
+		isHand = self.predict.doPrediction(1, "knn", problem_hand, "hands", self.goalImg)
+		if(isHand == 1):
+			self.predict.doPrediction(1, "knn", problem_sign, "rock", self.goalImg)
                 if cv.WaitKey(10) >= 0:
                     break
 
