4f6722034fa3585757577bbdfd7126982ceb47bd
silvia.laura.pintea <silvia.laura.pintea@fb641738-5793-2f27-248a-c372f8ce5f8b>
2010-06-18 17:55:59 +0000

    predicts the class of an input image

---
+++ predictSign.py
@@ -0,0 +1,151 @@
+# Combines the hand-detection part with sign classification
+#
+# Input:
+# - pca   = an object of the class "eigenHands"
+# - gabor = an object of the class "gaborFilters"
+# - image = image for which the prediciton needs to be done 	
+#
+# Output:
+# - doPrediction(type,model,problem,what,image) => predicts the class of "image"; 
+#						   model   -- "svm"/"knn"
+#						   problem -- is the mlpy problem stored in "classi_models"
+#						   what    -- "rock" for "rock & paper & scissors";
+#							   -- "hands" for "hands vs garbage"
+#						   type    -- 1 = on original images, 
+#							      2 = on eigen-hands of original images
+#							      3 = on convolved Gabors + original image
+#							      4 = just on convolved Gabors
+# - storeModel(model, theSign, onImg) => stores a model for a model ("svn"/"knn"); 
+#					 theSign -- "rock" for "rock & paper & scissors";
+#					         -- "hands" for "hands vs garbage"
+#					 type    -- 1 = on original images, 
+#					            2 = on eigen-hands of original images
+#						    3 = on convolved Gabors + original image
+#					            4 = just on convolved Gabors
+# - loadModel(model, type)            => load the model given my model ("svn"/"knn") and type (1/2/3/4)
+# - preprocessImg(image, type)        => preprocesses the test image corresponding to the type (1/2/3/4)
+# 
+import sys
+import urllib
+import re
+import copy
+import math
+import random
+import cv
+import numpy 
+import os
+import glob
+import mlpy
+import pickle
+from eigenHands import *
+from gaborFilters import *
+from preprocessing import *
+from classifyHands import *
+
+class predictSign:
+	def __init__(self, size):
+		self.pca      = eigenHands(size)
+		self.gabor    = gaborFilters(False, size)
+		self.classify = classifyHands(False, size)
+		self.prep     = preprocessing(size)
+	#________________________________________________________________________
+	#store the models
+	def storeModel(self, model, theSign, onImg):
+		if(model == "svm"):
+			#0) get training data data and the labels			
+			indexs,labels,train = self.classify.getDataLabels(onImg, theSign, False)
+
+			#1) initialize the svm and compute the model
+			if(onImg == 1): #for full images 		
+				problem = mlpy.Svm(kernel='gaussian', C=1.0, kp=0.1, tol=0.001, eps=0.001, maxloops=1000, opt_offset=True)
+			elif(onImg == 2): #for PCAed images
+				problem = mlpy.Svm(kernel='gaussian', C=1.0, kp=0.1, tol=0.001, eps=0.001, maxloops=1000, opt_offset=True)
+			elif(onImg == 3): #for gabore filters
+				problem = mlpy.Svm(kernel='polynomial', kp=0.3, C=1.0, tol=0.0001, eps=0.0001, maxloops=1000, opt_offset=True)
+
+			#2) shuffle input data to do the 10-fold split 
+			shuffle(indexs)
+			labels = labels[indexs]
+			train  = train[indexs,:] 
+
+			#3) generate the svm model
+			learned   = problem.compute(train, labels)
+			modelFile = open("classi_models/"+str(self.pca.sizeImg)+"Knn_problem"+str(onImg)+".dat", "wb")
+			pickle.dump(problem, modelFile)
+			modelFile.close()			
+		else:
+			#0) get training data data and the labels
+			indexs,labels,train = self.classify.getDataLabels(onImg, theSign, True)
+
+			#1) initialize the svm and compute the model
+			problem = mlpy.Knn(5, dist='se')
+
+			#2) shuffle input data to do the 10-fold split 
+			shuffle(indexs)
+			labels = labels[indexs]
+			train  = train[indexs,:] 
+
+			#3) generate the Knn model
+			learned   = problem.compute(train, labels)
+			modelFile = open("classi_models/"+str(self.pca.sizeImg)+"Knn_problem"+str(onImg)+".dat", "wb")
+			pickle.dump(problem, modelFile)
+			modelFile.close()
+	#________________________________________________________________________
+	#just load model
+	def loadModel(self, model, zaType):
+		if(model == "svm"):
+			modelFile = open("classi_models/"+str(self.pca.sizeImg)+"SVM_problem"+str(zaType)+".dat", "r")
+		else:
+			modelFile = open("classi_models/"+str(self.pca.sizeImg)+"Knn_problem"+str(zaType)+".dat", "r")
+		problem = pickle.load(modelFile)
+		modelFile.close()
+		return problem
+	#________________________________________________________________________
+	#does the prediction over the given image
+	def doPrediction(self, zaType, model, problem, what, image):
+		totalTime = 0
+		if(model == "svm"):
+			classifyWhat = {"hands":{"1":"hands", "-1":"garb"}, "rock": {"1":"rock", "-1":"paper or scissors"}, "paper":{"1":"paper", "-1":"scissors"}}
+		else:
+			classifyWhat = {"hands":{"1":"hands", "-1":"garb", "0":"..don't know"}, "rock":{"1":"rock", "2":"paper", "3":"scissors", "0":"..don't know"}}
+		zaTime     = cv.GetTickCount() 
+		testImg    = self.preprocessImg(image, zaType)
+		prediction = problem.predict(testImg)
+		zaTime     = cv.GetTickCount() - zaTime
+	    	totalTime += zaTime/(cv.GetTickFrequency()*1000.0)
+			
+		print "it is a..."+str(classifyWhat[str(what)][str(prediction[0])])+" >>> prediction time/image %gms" % totalTime
+
+	#________________________________________________________________________
+	def preprocessImg(self, image, zaType):
+		#1) resize the image to the needed size
+		resizeImg = cv.CreateMat(self.pca.sizeImg, self.pca.sizeImg, cv.CV_8UC1)
+		cv.Resize(image, resizeImg, interpolation = cv.CV_INTER_AREA)	
+
+		#2) reshape the image as a row
+		imgMat = cv.Reshape(resizeImg, 0, 1)
+
+		if(zaType == 1): # original images
+			numpyImg = self.pca.cv2array(imgMat, True)
+			return numpyImg
+		elif(zaType == 2): # PCA over the original images
+			"""do pca on 1 image somehow"""
+		elif(zaType == 3): # convolve with 8 kernels and add the original image
+			return self.prep.doSmallManyGabors(imgMat, "", 0, False, False)
+		elif(zaType == 4): # convolve with 8 kernels and add the original image
+			return self.prep.doManyGabors(imgMat, "", 0, 0, False, False)
+	
+#________________________________________________________________________
+	
+
+
+
+
+
+
+
+
+
+
+
+
