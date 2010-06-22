9e945fc59eb9b670ed6702bafb071fe7154f1354
silvia.laura.pintea <silvia.laura.pintea@fb641738-5793-2f27-248a-c372f8ce5f8b>
2010-06-22 17:20:49 +0000

    run nao with threads

--- runNao.py
+++ runNao.py
@@ -1,48 +1,56 @@
 import os
 import sys
 import random
-
 path = 'os.environ.get("AL_DIR")'
 home = 'os.environ.get("HOME")'
+
 if path == "None":
         print "the environment variable AL_DIR is not set, aborting..."
         sys.exit(1)
 else:
         #import naoqi lib
-        alPath = "C:\Program Files\Aldebaran\Choregraphe 1.6.13\lib"
+	alPath = "/data/Documents/semester2/AIProject/nao/lib"
+        #alPath = "C:\Program Files\Aldebaran\Choregraphe 1.6.13\lib"
         sys.path.append(alPath)
         import naoqi
         from naoqi import ALBroker
         from naoqi import ALModule
         from naoqi import ALProxy
-        from naoqi import ALBehavior 
-        
+        from naoqi import ALBehavior         
 BASEPATH = "/home/nao/behaviors/"
 
 import cv
 from skinFinder import *
+from naoMotions import *
+import thread
 
 #_____________________________________________________________________
-#1) CONNECT TO NAO
-goNao = Gesture()
+#1) CONNECT TO NAO AND INITIALIZE CLASS SKIN
 skin  = detectSkin()
+goNao = Gesture()
 
-#2) START WITH A DEMO
+#2) MAKE MULTI THREADS SO THE SKIN FINDER AND NAO INITIALIZE AND RUN IN PARALLEL
+try:
+	thread.start_new_thread(skin.findSkin, ())
+except:
+	print "error for skin finder"
+
 goNao.naoBehaviors("demo")
-
 for i in range(0,3):
-	#3) LET'S PLAY NOW
+	thread.start_new_thread(skin.reinitGame, ())
+
+	#3) LET'S PLAY NOW___________________
 	goNao.naoBehaviors("play")
-
-
-	#3) LET'S PLAY NOW
-	goNao.naoBehaviors("play")
-
-	#4) PREDICT WHO PLAYED WHAT
+	
+	#4) PREDICT WHO PLAYED WHAT_________________	
 	goNao.naoBehaviors("move")
+
 	moves = {0:"rock", 1:"paper", 2:"scissors"}
-	if(skin.maximum == 10):
-		if(skin.maximum == moves(goNao.naoMove)) or (): #0=rock, 1=paper, 2=scissors
+
+	print str(skin.maximum)+" "+str(skin.maxnr)
+
+	if(skin.maxnr >= 2 and skin.maximum != "none" and skin.maximum != "garb"):
+		if(skin.maximum == moves[int(goNao.naoMove)]): #0=rock, 1=paper, 2=scissors
 			goNao.naoBehaviors("equal")
 		elif(skin.maximum == "rock" and goNao.naoMove == 1): #rock>scissors>paper>rock
 			goNao.naoBehaviors("lost")
@@ -57,10 +65,8 @@ for i in range(0,3):
 		elif(skin.maximum == "scissors" and goNao.naoMove == 1):
 			goNao.naoBehaviors("won")
 		#5) RESET THE VARIABLES 
-		skin.predictions = {"rock":0, "paper":0, "scissors":0, "garb":0}
-		skin.maximum     = ""
-
-
+		thread.start_new_thread(skin.reinitGame, ())
+	
 
 
 
